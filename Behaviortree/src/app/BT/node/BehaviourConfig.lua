0717e271c52e2b5454b466e47fb4c98e5c72f8de
fuxing <563476223@qq.com>
2018-03-23 11:59:23 +0800

    基础目录

---
+++ Behaviortree/src/app/BT/node/BehaviourConfig.lua
@@ -0,0 +1,11 @@
+--======================================================
+-- Author: 付星
+-- Purpose:
+-- Date: 2018-03-23 11:57:44
+--======================================================
+local config = {}
+
+config.RUNNING       = 'RUNNING'
+config.READY		 = 'READY'
+
+return config
---
+++ Behaviortree/src/app/BT/node/BehaviourNode.lua
@@ -0,0 +1,12 @@
+--======================================================
+-- Author: 付星
+-- Purpose:行为根节点
+-- Date: 2018-03-23 11:56:12
+--======================================================
+local BehaviourNode = class("BehaviourNode")
+
+function BehaviourNode:ctor(  )
+	
+end
+
+return  BehaviourNode
